7f837ea313c35d970c7b2742558ed1d3ff675719
Maino24 <mg02.gabriele@gmail.com>
2022-11-08 16:47:29 +0100

    creato array bici

---
+++ snack 3/main.js
@@ -0,0 +1,11 @@
+/*Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore utilizzando destructuring e template literal */!SECTION
+
+arrayBici = [
+
+    { name: 'Scott', peso: 14},
+    { name: 'Orbea', peso: 15 },
+    { name: 'Ktm', peso: 16 },
+    { name: 'Cannondale', peso: 18 },
+    { name: 'Cervelo', peso: 14 },
+
+]
