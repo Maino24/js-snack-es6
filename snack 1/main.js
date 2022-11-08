ccb258c7dcc1260765b2e7a67c41141fcdd94405
Maino24 <mg02.gabriele@gmail.com>
2022-11-08 10:32:39 +0100

    snack  1 punto 2

--- snack 1/main.js
+++ snack 1/main.js
@@ -3,12 +3,14 @@ Creiamo un nuovo array contenente i valori che hanno la posizione compresa tra i
 Eseguiamo questo esercizio prima con forEach e poi con filter.
 */
 
-//con forEach
+
 
 const myArray = ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'];
 
  let min = parseInt(prompt(`inserisci un numero tra 0 e 6`))
  let max = parseInt(prompt(`inserisci un numero maggiore di quello precedente ma inferiore o uguale A 6`))
+
+//con forEach
 
 function estrazioneNomi(min, max){
    const arrayNuovo = []
@@ -23,9 +25,22 @@ console.log(arrayNuovo)
 }
 estrazioneNomi(min, max)
 
+
+
 //con filter
 
-
+function estrazioneNomi(min, max){
+    
+   arrayNuovo =  myArray.filter((element, index) => {
+     if( index > min  && index < max){
+         return true
+        
+     }
+    
+ })
+ console.log(arrayNuovo)
+ }
+ estrazioneNomi(min, max)
 
 
 
