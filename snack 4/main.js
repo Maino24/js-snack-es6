81ce55ba4516a794d27c882e5ac0ae26acb6c4b0
Maino24 <mg02.gabriele@gmail.com>
2022-11-09 09:55:48 +0100

    array

---
+++ snack 4/main.js
@@ -0,0 +1,12 @@
+
+
+const teams = [
+
+    { nome: `PSG`, punti: 0, falli: 0,},
+    { nome: `Ajax`, punti: 0, falli: 0,}
+    { nome: `Manchester City`, punti: 0, falli: 0,}
+    { nome: `Real Madrid`, punti: 0, falli: 0,}
+
+
+]
+
