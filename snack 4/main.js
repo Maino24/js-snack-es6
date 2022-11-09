40030710ab853102442bf2127506279be90b2735
Maino24 <mg02.gabriele@gmail.com>
2022-11-09 10:02:55 +0100

    funzioni numeroRandom

--- snack 4/main.js
+++ snack 4/main.js
@@ -3,10 +3,32 @@
 const teams = [
 
     { nome: `PSG`, punti: 0, falli: 0,},
-    { nome: `Ajax`, punti: 0, falli: 0,}
-    { nome: `Manchester City`, punti: 0, falli: 0,}
-    { nome: `Real Madrid`, punti: 0, falli: 0,}
+    { nome: `Ajax`, punti: 0, falli: 0,},
+    { nome: `Manchester City`, punti: 0, falli: 0,},
+    { nome: `Real Madrid`, punti: 0, falli: 0,},
 
 
 ]
 
+//let obj = {
+//    nome: `PSG`,
+//    punti: 0,
+//    falli: 0,
+    
+//}
+
+function numeroRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+
+}
+
+const teamsCopia = teams.map( (elem) => {
+
+    elem.punti = numeroRandom(1, 100);
+    elem.falli = numeroRandom(1, 100);
+
+    return elem
+
+})
+
+console.log( teamsCopia )
